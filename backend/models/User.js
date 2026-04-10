47bec0c15aa7c6e05507223af1462d37792162b9
{your_name} <{your_email}>
2026-04-10 11:38:55 +0530

    Save progress before frontend vanilla CSS rewrite

--- backend/models/User.js
+++ backend/models/User.js
@@ -31,14 +31,9 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function(next) {
-  try {
-    if (!this.isModified('password')) return next();
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  } catch (error) {
-    next(error);
-  }
+userSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
 // Compare password method
